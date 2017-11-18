f5800d172247b46ae4d97443dacb48fe9adbc0a0
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-18 11:54:14 -1000

    get all and get all w relations

--- server/routes/users.js
+++ server/routes/users.js
@@ -6,10 +6,34 @@ const User = db.user;
 const Message = db.message;
 const Status = db.status;
 
+
+//api/users gets you JUST the users
+
 router.get('/', (req, res) => {
   return User.findAll()
   .then(users => {
     return res.json(users);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+});
+
+//api/users/all gets you all users + their related tables
+
+router.get('/all', (req, res) => {
+  return User.findAll({
+    include: [
+      { model: Message, as: 'shader' },
+      { model: Message, as: 'victim' },
+      { model: Status, as: 'status' }
+    ]
+  })
+  .then((users) => {
+    return res.json(users);
+  })
+  .catch((err) => {
+    console.log(err);
   });
 });
 
