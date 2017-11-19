b3f987cebcdeca028d368e3cfd589576676f1c2e
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-11-18 14:36:34 -1000

    adds bcrypt, post to users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
 const db = require('../models');
 const User = db.user;
 const Message = db.message;
@@ -24,9 +27,9 @@ router.get('/', (req, res) => {
 router.get('/all', (req, res) => {
   return User.findAll({
     include: [
-      { model: Message, as: 'shader' },
-      { model: Message, as: 'victim' },
-      { model: Status, as: 'status' }
+      { model: Message, as: 'offense' },
+      { model: Message, as: 'defense' },
+      { model: Status, as: 'user_status' }
     ]
   })
   .then((users) => {
@@ -34,6 +37,25 @@ router.get('/all', (req, res) => {
   })
   .catch((err) => {
     console.log(err);
+  });
+});
+
+router.post('/', (req, res) => {
+  //note: req.body will need an emoji field w/ emoji id.
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      User.create({
+        username: req.body.username,
+        password: hash,
+        emoji_id: req.body.emoji
+      })
+      .then((user) => {
+        return res.json(user);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    });
   });
 });
 
